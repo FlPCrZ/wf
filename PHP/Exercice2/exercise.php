57f83567ba3a36735992592be445e7a60372a4b2
koteknik <ko.ktk@pm.mek>
2018-10-19 11:19:50 +0200

    Exercice 2

--- PHP/Exercice2/exercise.php
+++ PHP/Exercice2/exercise.php
@@ -2,4 +2,15 @@
 $password;
 $salt;
 
-$saltedPassword = '';
+$temp1 = substr(
+    $password, 
+    0, 
+    floor(strlen($password) / 2) + (strlen($password) / 2)
+    );
+
+$temp2 = substr(
+    $password, 
+    floor(strlen($password) / 2)
+    );
+
+$saltedPassword = "$temp1$salt$temp2";
