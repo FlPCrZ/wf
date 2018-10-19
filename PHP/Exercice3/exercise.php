14897a877a1cb864cebcb49bd81c583d85c34dc8
koteknik <ko.ktk@pm.mek>
2018-10-19 12:17:02 +0200

    Exercice3

--- PHP/Exercice3/exercise.php
+++ PHP/Exercice3/exercise.php
@@ -1,4 +1,21 @@
 <?php
-
 $input;
 $winner;
+$pointsA = 0;
+$pointsB = 0;
+
+forEach($input as $row) {
+    $cardA = $row[0];
+    $cardB = $row[1];
+    if ($cardA > $cardB) {
+        $pointsA += 1;
+    } else if ($cardA < $cardB) {
+        $pointsB += 1;
+    }
+}
+
+if ($pointsA > $pointsB){
+    $winner = 'A';
+} else if ($pointsA < $pointsB) {
+    $winner = 'B';
+}
